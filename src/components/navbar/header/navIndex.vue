55aa77363300da887346ec2319f3d165117a498e
zhangsuqi <zsq15135068239@163.com>
2024-06-07 14:08:26 +0800

    1

--- src/components/navbar/header/navIndex.vue
+++ src/components/navbar/header/navIndex.vue
@@ -6,7 +6,7 @@ import { useAppConfigStore } from '@/stores/appConfig'
 const { $state, setMenuCollapsed } = useAppConfigStore()
 </script>
 <template>
-  <a-layout-header style="background: #fff; margin: 0">
+  <a-layout-header style="background: #fff; margin: 0; display: flex;justify-content: space-between;">
     <menu-unfold-outlined
       v-if="$state.collapsed"
       class="trigger"
