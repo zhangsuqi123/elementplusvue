e3f748dfe832c8fe3645bcad74224fface61f691
张苏旗 <zsq15135068239@163.com>
2024-06-15 12:28:31 +0800

    做一个mac和windows的加密key读取

---
+++ src/libs/crypto-web.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import crypto from 'crypto-js';
+
+const key = crypto.enc.Hex.parse(crypto_key); // 将字符串解析为十六进制数作为密钥
+const iv = crypto.enc.Hex.parse(crypto_key); // 将字符串解析为十六进制数作为 IV
+
+class Cryptos {
+  encode(data) {
+    const encrypted = crypto.AES.encrypt(data, key, {
+      iv,
+      mode: crypto.mode.CBC,
+      padding: crypto.pad.Pkcs7,
+    });
+    return encrypted.toString();
+  }
+  decode(encrypted) {
+    const decrypted = crypto.AES.decrypt(encrypted, key, {
+      iv,
+      mode: crypto.mode.CBC,
+      padding: crypto.pad.Pkcs7,
+    });
+    return decrypted.toString(crypto.enc.Utf8);
+  }
+}
+
+export default new Cryptos();
--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,40 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver, AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { VxeTableResolver } from '@vxecli/import-unplugin-vue-components';
 import dotenv from 'dotenv';
+import path from 'path-browserify';
+import fs from 'fs';
 
+// 根据环境加载相应的 .env 文件
 const envFiles = {
   development: '.env.development',
   production: '.env.production'
 };
 
+let str;
+try {
+  str = fs.readFileSync('/opt/.crypto_key', 'utf8').trim();
+} catch (error) {
+  // 获取用户主目录
+  const userProfile = process.env.USERPROFILE;
+  // 获取用户的桌面路径
+  const desktopDir = path.join(userProfile, 'Desktop');
+
+  // 指定你要读取的文件名
+  const filename = 'crypto';
+
+  // 构建文件的完整路径
+  const filePath = path.join(desktopDir, filename);
+  str = fs.readFileSync(filePath, 'utf8').trim();
+}
+
+// 加载环境变量
 dotenv.config({ path: envFiles[process.env.NODE_ENV] });
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    crypto_key: JSON.stringify(str)
+  },
   plugins: [
     vue(),
     vueJsx(),
